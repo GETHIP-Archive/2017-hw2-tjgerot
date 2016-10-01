ff9ef867a501d263e95cdbd6126764b368533fd8
Thomas Gerot <tjgerot@gmail.com>
2016-10-01 11:16:38 -0500

    :thumbsup: Have fun!

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-tjgerot/master/thomas.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
-    $.each(data.nickNames, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+    $.each(data.strengths, function(key, value){
+      $("#strengths").append("<li>" + value.strength + "</li>");
     });
   });
 });
